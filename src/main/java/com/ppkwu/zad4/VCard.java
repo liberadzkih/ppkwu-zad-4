9ab3433a39fb2b32aac0f4ff327d3f70989689da
liberadzkih <liberadzkih@gmail.com>
2020-01-20 22:31:03 +0100

    VCard generator

--- src/main/java/com/ppkwu/zad4/VCard.java
+++ src/main/java/com/ppkwu/zad4/VCard.java
@@ -8,10 +8,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 /*
@@ -33,16 +41,84 @@ import java.util.List;
 @RestController
 public class VCard {
 
+    @RequestMapping("VCard")
+    public String startPageVCard() throws IOException {
+        String filePath = "first_page.html";
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
+    }
+
     @RequestMapping("VCard/{name}")
     public String getVCard(@PathVariable String name) throws IOException {
-        name.replace(" ", "+");
 
+        String url = buildHttpPath(name);
+        HashMap<String, List<String>> hashMap =  makeHashMap(url);
+        List<String> pictures = hashMap.get("pictures");
+        List<String> names = hashMap.get("names");
+        List<String> titles = hashMap.get("titles");
+        List<String> extraInfo = hashMap.get("info");
+        int listSize = pictures.size();
+        List<String> VCard = new ArrayList<>();
+
+        for (int i = 0; i < listSize; i++) {
+            VCard.add("<center>");
+            VCard.add("<br>");
+            VCard.add("<img src='" + pictures.get(i) + "'>");
+            VCard.add("<br>");
+            VCard.add(titles.get(i) + " " + names.get(i) + "<br>" + extraInfo.get(i));
+            VCard.add("<form action='http://localhost:8080/VCard/" + name + "/"+ i + "' />");
+            VCard.add("     <input id='" + i + "' type='submit' value='Wygeneruj VCard' />");
+            VCard.add("</form>");
+            VCard.add("</center>");
+            VCard.add("<hr>");
+        }
+
+        return VCard.toString().replace("[", "").replace("]", "").replace(",", "");
+    }
+
+    @RequestMapping("VCard/{name}/{number}")
+    public String generateVCard(@PathVariable String name, @PathVariable int number) throws IOException {
+        String url = buildHttpPath(name);
+        HashMap<String, List<String>> hashMap =  makeHashMap(url);
+        List<String> pictures = hashMap.get("pictures");
+        List<String> names = hashMap.get("names");
+        List<String> titles = hashMap.get("titles");
+        List<String> extraInfo = hashMap.get("info");
+        generateCard(names.get(number), titles.get(number), extraInfo.get(number));
+        return names.get(number) + " VCard generated!";
+    }
+
+    public void generateCard(String name, String title, String info) throws IOException {
+
+        String firstname = name.substring(0, name.indexOf(' '));
+        String surname = name.substring(name.indexOf(' ')+1);
+        File file = new File(firstname + "_" + surname + "_vCard" + ".vcf");
+        file = file.getAbsoluteFile();
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write("BEGIN:VCARD\r\n");
+        bufferedWriter.write("VERSION:4.0\r\n");
+        bufferedWriter.write("N:" + surname + ";" + firstname + ";;" + title + "\r\n");
+        bufferedWriter.write("FN:" + firstname + " " + surname + "\r\n");
+        bufferedWriter.write("ORG:" + info + "\r\n");
+        bufferedWriter.write("END:VCARD");
+        bufferedWriter.close();
+    }
+
+    public String buildHttpPath(String name){
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("https://adm.edu.p.lodz.pl/").append("user/users.php?search=");
         stringBuilder.append(name);
+        return stringBuilder.toString();
+    }
 
-        String url = stringBuilder.toString();
-
+    public HashMap<String, List<String>> makeHashMap(String url) throws IOException {
         Document document;
         document = Jsoup.parse(new URL(url), 10000);
 
@@ -52,12 +128,14 @@ public class VCard {
         Elements userName = userDetails.select("h3");
         Elements userAcademicTitle = userDetails.select("h4");
         Elements userExtraInfo = userDetails.select("div.extra-info");
+        Elements userPublicProfile = userDetails.select("a.fullprofile-link");
 
 
         List<String> pictures = new ArrayList<>();
         List<String> names = new ArrayList<>();
         List<String> titles = new ArrayList<>();
         List<String> extraInfo = new ArrayList<>();
+        List<String> publicProfiles = new ArrayList<>();
 
         for (Element e : userPicture)
             pictures.add(e.attr("src"));
@@ -67,17 +145,17 @@ public class VCard {
             titles.add(e.text());
         for (Element e : userExtraInfo)
             extraInfo.add(e.text());
+        for (int i=0; i<userPublicProfile.size(); i++)
+            if (i % 2 == 0)
+                publicProfiles.add(userPublicProfile.get(i).attr("href"));
 
-        int listSize = pictures.size();
-        List<String> VCard = new ArrayList<>();
-
-        for (int i = 0; i < listSize - 1; i++) {
-            String pic = "<img src='" + pictures.get(i) + "'>";
-            VCard.add(pic + "\t\t" + names.get(i) + "\n" + titles.get(i) + "\n" + extraInfo.get(i));
-            VCard.add("<br></br>");
-        }
-
-        return VCard.toString().replace("[", "").replace("]", "").replace(",", "");
+        HashMap<String, List<String>>  lists = new HashMap<>();
+        lists.put("pictures", pictures);
+        lists.put("names", names);
+        lists.put("titles", titles);
+        lists.put("info", extraInfo);
+        lists.put("profiles", publicProfiles);
+        return lists;
     }
 
 }
